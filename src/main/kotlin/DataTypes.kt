c7db9d4527e70989f4f12ec0c74e426e767986b6
yuumuu <haidar.alhazen93@gmail.com>
2026-09-24 23:17:01 +0700

    feat: add HelloWorld and DataTypes Kotlin examples

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val d: Int
+    d = 3
+
+    val e: String = "hello"
+    println(e)
+
+    val byteValue: Byte = 100
+    val shortValue: Short = 1000
+    val intValue: Int = 100000
+    val longValue: Long = 1000000000L
+
+    val uByteValue: UByte = 255u
+    val uShortValue: UShort = 65535u
+    val uintValue: UInt = 4294967295u
+    val ulongValue: ULong = 18446744073709551615u
+
+    val floatValue: Float = 3.14f
+    val doubleValue: Double = 3.141592653589793
+
+    val boolValue: Boolean = true
+    val charValue: Char = 'A'
+
+    println("All basic types declared successfully")
+}
---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Hello, world!")
+    // Hello, world!
+}
+
+fun main() {
+    val customers = 10
+    println("There are $customers customers")
+}
+
+fun main() {
+    val name: String = "Alice"
+    val age: Int = 25
+    println("My name is $name and I am $age years old")
+}
